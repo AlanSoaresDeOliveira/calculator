51a69cb5cc1e9e03a876c77f31c52bddace8cd11
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-09-07 10:29:00 -0300

    Create methods to Buttons

--- App.tsx
+++ App.tsx
@@ -5,27 +5,37 @@ import Display from './src/components/Display';
 
 const App = () => {
   const [displayValue, setDisplayValue] = useState(0);
+
+  const addDigit = (n) => {
+    setDisplayValue(n);
+  };
+
+  const clearMemory = () => {
+    setDisplayValue(0);
+  };
+
+  const setOperation = (operation) => {};
   return (
     <View style={styles.container}>
       <Display value={displayValue} />
       <View style={styles.button}>
-        <Button label="AC" />
-        <Button label="/" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="*" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" />
-        <Button label="0" />
-        <Button label="." />
-        <Button label="=" />
+        <Button label="AC" triple onClick={clearMemory} />
+        <Button label="/" operation onClick={() => setOperation('/')} />
+        <Button label="7" onClick={() => addDigit(7)} />
+        <Button label="8" onClick={() => addDigit(8)} />
+        <Button label="9" onClick={() => addDigit(9)} />
+        <Button label="*" operation onClick={() => setOperation('*')} />
+        <Button label="4" onClick={() => addDigit(4)} />
+        <Button label="5" onClick={() => addDigit(5)} />
+        <Button label="6" onClick={() => addDigit(6)} />
+        <Button label="-" operation onClick={() => setOperation('-')} />
+        <Button label="1" onClick={() => addDigit(1)} />
+        <Button label="2" onClick={() => addDigit(2)} />
+        <Button label="3" onClick={() => addDigit(3)} />
+        <Button label="+" operation onClick={() => setOperation('+')} />
+        <Button label="0" double onClick={() => addDigit(0)} />
+        <Button label="." onClick={() => addDigit('.')} />
+        <Button label="=" operation onClick={() => setOperation('=')} />
       </View>
     </View>
   );
