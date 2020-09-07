11cd24f25499c19f772fbdfa88e373d538f91512
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-09-07 10:35:02 -0300

    Add news methods

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -37,7 +37,7 @@ export default (props) => {
   }
 
   return (
-    <TouchableHighlight onPress={props.onClick}>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
       <Text style={stylesButton}>{props.label}</Text>
     </TouchableHighlight>
   );
